c1303b69b887d92d4f3fb0b6058dd28e40e7c77b
sainyaVishwakarma <104253247+sainyaVishwakarma@users.noreply.github.com>
2023-07-15 10:59:50 +0530

    Add files via upload

---
+++ DemoCollections/DemoApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using HR;
+List<Person> student=new List<Person>();
+student.Add(new Person{FirstName="sss",LastName="hdhfk"});
+
+Dictionary<string,Person> p=new Dictionary<string, Person>();
+p.Add("ceo",new Person{FirstName="vcjjvh",LastName="xhgxyvf"});
+Person pceo=p["ceo"];
+
+Stack<string> callerStudent=new Stack<string>();
+callerStudent.Push("sainya");
+string theStudent=callerStudent.Pop();
